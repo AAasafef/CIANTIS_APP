e0b5d8559a7dad1110799612872b6a395bea2205
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-08-26 09:09:48 -0400

    Use FragmentActivity for biometric authentication

--- android/app/src/main/kotlin/com/example/ciantis_clean/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ciantis_clean/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.ciantis_clean
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
